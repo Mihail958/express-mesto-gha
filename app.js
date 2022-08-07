0087db0fe16d2974624078282974936e63e50b60
Mihail <misha95814@gmail.com>
2022-08-07 19:27:40 +0300

    13.1.12

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ const bodyParser = require('body-parser');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb ', {
+   useNewUrlParser: true
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '62ef9d2256f5d69128e6fc61' 
+    _id: '62ef9d2256f5d69128e6fc61',
   };
 
   next();
-}); 
+});
 
 app.use(routesUsers);
 app.use(routesCards);
@@ -25,11 +30,6 @@ app.use(routesCards);
 app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
-
-mongoose.connect('mongodb://localhost:27017/mestodb ', {
-   useNewUrlParser: true
-});
-
 
 app.listen(PORT, () => {
   console.log(`Рабочий порт: ${PORT}`);
