4b5a3aa4ba59230aa2bfe52d5dcfb50cea85cc45
Mihail <misha95814@gmail.com>
2022-08-07 19:42:00 +0300

    13.1.13

--- app.js
+++ app.js
@@ -2,19 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routesUsers = require('./routes/users');
 const routesCards = require('./routes/cards');
-const bodyParser = require('body-parser');
-
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb ', {
-   useNewUrlParser: true
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
@@ -31,6 +24,12 @@ app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
+
 app.listen(PORT, () => {
-  console.log(`Рабочий порт: ${PORT}`);
+  console.log(`Приложение слушает порт: ${PORT}`);
 });
