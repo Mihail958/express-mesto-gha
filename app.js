384c9e4166b3da7439c85b8235c06a7356bb2cbd
Mihail <misha95814@gmail.com>
2022-08-07 19:04:40 +0300

    13.1.10

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use((req, res) => {
 });
 
 mongoose.connect('mongodb://localhost:27017/mestodb ', {
-  useNewUrlParser: true
+  // useNewUrlParser: true
 });
 
 app.use((req, res, next) => {
--- consts/error_codes.js
+++
@@ -1,3 +0,0 @@
-module.exports.BAD_REQUEST_CODE = 400;
-module.exports.PAGE_NOT_FOUND_CODE = 404;
-module.exports.COMMON_ERROR_CODE = 500;
