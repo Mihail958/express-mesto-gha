afbf5651836d4235e43505e6463d4b07bbecf863
Mihail <misha95814@gmail.com>
2022-08-16 18:31:59 +0300

    14.1.2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,24 @@
 const jwt = require('jsonwebtoken');
-const statusCodes = require('../utils/statusCodes');
+const Unauthorized = require('../errors/Unauthorized');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res(statusCodes.ERROR_CODE_401).send({ message: 'Необходима авторизация' });
+    next(new Unauthorized('Требуется авторизация'));
   }
+
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(token, 'some-secret-key');
-    req.user = payload;
-    next();
   } catch (err) {
-    return res.status(statusCodes.ERROR_CODE_401).send({ message: 'Необходима авторизация' });
+    return new Unauthorized('Требуется авторизация');
   }
+
+  req.user = payload;
+
+  next();
 };
